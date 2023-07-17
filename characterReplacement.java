8a18559ca7e3a08399a16904e1738afd7586eab7
kjk0122 <juke9548@gmail.com>
2023-07-17 10:38:03 +0900

    틀만 만들기

---
+++ characterReplacement.java
@@ -0,0 +1,12 @@
+public class characterReplacement {
+    public static void main(String[] args) {
+        String word = "ABCABC";
+        int num = 33;
+        int gcd = characterReplacement(word, num);
+        System.out.println(gcd);
+    }
+
+    public static int characterReplacement(String s, int k) {
+    return 1;
+    }
+}
