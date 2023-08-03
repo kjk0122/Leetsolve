92186767695f05dc2c831e13d37d20223756a2a9
kjk0122 <juke9548@gmail.com>
2023-08-03 16:18:47 +0900

    일단 올리기

---
+++ KidsWithCandies.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class KidsWithCandies {
+    public static void main(String[] args) {
+        int[] nums = {11, 7, 11, 2};
+        int target = 9;
+        List<Boolean>[] answer = kidsWithCandies(nums, target);
+        System.out.println(Arrays.toString(answer));
+    }
+
+    public static  List<Boolean>[] kidsWithCandies(int[] nums, int target) {
+
+        return new List[]{} ; // 답 없음;;
+    }
+}
