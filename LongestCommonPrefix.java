fc9cd7ad022d7005adb1e679097b853833a1e6c0
kjk0122 <juke9548@gmail.com>
2023-09-06 18:31:49 +0900

    수정

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -7,18 +7,19 @@ public class LongestCommonPrefix {
     }
 
     public static String longestCommonPrefix(String[] strs) {
-        Arrays.sort(strs);
-        String s1 = strs[0];
-        String s2 = strs[strs.length - 1];
-        int idx = 0;
-        while (idx < s1.length() && idx < s2.length()) {
-            if (s1.charAt(idx) == s2.charAt(idx)) {
-                idx++;
-                System.out.println(idx);
-            } else {
-                break;
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0]; // 첫 번째 문자열을 기준으로 설정합니다.
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
             }
         }
-        return s1.substring(0, idx);
+        return prefix;
     }
 }
