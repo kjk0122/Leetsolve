f205ca14a7f911f4f1a672a344f40a6024991e3c
kjk0122 <juke9548@gmail.com>
2023-09-07 14:23:31 +0900

    이름 변경 및 문제 푼것 올림

---
+++ Search.java
@@ -0,0 +1,13 @@
+import java.util.stream.IntStream;
+
+public class Search {
+    public static void main(String[] args) {
+        int[] nums = {3, 5, 6, 0, 0, 1, 4};
+        int target = 2;
+        System.out.println(search(nums, target));
+    }
+
+    public static boolean search(int[] nums, int target) {
+        return IntStream.of(nums).anyMatch(x -> x == target);
+    }
+}
