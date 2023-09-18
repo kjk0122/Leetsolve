ae2ecefaed92c7ba84774cfa23ae34b1d92e6f89
kjk0122 <juke9548@gmail.com>
2023-09-18 13:05:11 +0900

    문제 푼것 추가

---
+++ MyPow.java
@@ -0,0 +1,11 @@
+public class MyPow {
+    public static void main(String[] args) {
+        double x=2.000;
+        int n = 10;
+        System.out.println(myPow(x, n));
+    }
+
+    public static double myPow(double x, int n) {
+        return 2.12;
+    }
+}
