1467bdf177d2c3abf4ed42cea237faa38f26aa48
kjk0122 <juke9548@gmail.com>
2023-09-25 18:44:36 +0900

    문제 못푼것 중간저장

---
+++ SwapPairs.java
@@ -0,0 +1,12 @@
+
+
+public class SwapPairs {
+    public static void main(String[] args) {
+        ListNode nums = new ListNode(1);
+        System.out.println(swapPairs(nums));
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        return head;
+    }
+}
