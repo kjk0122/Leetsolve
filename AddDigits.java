cd93047e6d46dd21306fc1611cbdf1f712f5c1b5
kjk0122 <juke9548@gmail.com>
2023-10-10 12:58:26 +0900

    업업

---
+++ AddDigits.java
@@ -0,0 +1,13 @@
+public class AddDigits {
+    public static void main(String[] args) {
+        int k=22323;
+        System.out.println(addDigits(k));
+    }
+
+    public static int addDigits(int num) {
+        while(num >9){
+            num = num %10+num /10;
+        }
+        return num;
+    }
+}
