37bc971bbad7af5c294c3f487945b6d099acfce6
kjk0122 <juke9548@gmail.com>
2023-10-13 12:06:28 +0900

    업업

--- IsUgly.java
+++ IsUgly.java
@@ -19,4 +19,15 @@ public class IsUgly {
         }
         return n == 1;
     }
+
+
+        public static boolean isUgly2(int n) {
+            if (n == 0)
+                return false;
+            for (int prime : new int[] {2, 3, 5}) //이런 형태의 for문은 본 적이 없다...매우 신기
+                while (n % prime == 0)
+                    n /= prime;
+            return n == 1;
+        }
+
 }
