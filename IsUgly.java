843e33ec4c1701bbd418c6168ca3dc2c260471e8
kjk0122 <juke9548@gmail.com>
2023-10-13 16:04:17 +0900

    업업

--- IsUgly.java
+++ IsUgly.java
@@ -1,7 +1,11 @@
 public class IsUgly {
     public static void main(String[] args) {
-        int k=22323;
-        System.out.println(isUgly(k));
+        int k = 1;
+        System.out.println(isUgly(7));
+        for(int i=1;i<10;i++){
+        System.out.println(nthUglyNumber(i));
+        }
+
     }
 
     public static boolean isUgly(int n) {
@@ -20,14 +24,36 @@ public class IsUgly {
         return n == 1;
     }
 
+    public static boolean isUgly2(int n) {
+        if (n == 0)
+            return false;
+        for (int prime : new int[]{2, 3, 5}) //이런 형태의 for문은 본 적이 없다...매우 신기
+            while (n % prime == 0)
+                n /= prime;
+        return n == 1;
+    }
 
-        public static boolean isUgly2(int n) {
-            if (n == 0)
-                return false;
-            for (int prime : new int[] {2, 3, 5}) //이런 형태의 for문은 본 적이 없다...매우 신기
-                while (n % prime == 0)
-                    n /= prime;
-            return n == 1;
+    public static int nthUglyNumber(int n) {
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+        int i2 = 0, i3 = 0, i5 = 0;
+
+        for (int i = 1; i < n; i++) {
+            int nextUgly = Math.min(ugly[i2] * 2, Math.min(ugly[i3] * 3, ugly[i5] * 5));
+            ugly[i] = nextUgly;
+
+            if (nextUgly == ugly[i2] * 2) {
+                i2++;
+            }
+            if (nextUgly == ugly[i3] * 3) {
+                i3++;
+            }
+            if (nextUgly == ugly[i5] * 5) {
+                i5++;
+            }
         }
+
+        return ugly[n - 1];
+    }
 
 }
