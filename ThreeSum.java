f715d61d4aa999c7c1e2d5932ebfd979dcf3d02a
kjk0122 <juke9548@gmail.com>
2023-10-23 19:34:31 +0900

    까먹지 않게 다른 방식 추가

--- ThreeSum.java
+++ ThreeSum.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class ThreeSum {
@@ -25,7 +27,42 @@ public class ThreeSum {
         }
         return result;
     }
+    public static List<List<Integer>> threeSum2(int[] nums) {
+        Arrays.sort(nums); // Sorted Array
+        List<List<Integer>> answer = new ArrayList<>();
 
+        if (nums.length < 3) {
+            return answer;
+        }
+
+        if (nums[0] > 0) {
+            return answer;
+        }
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; ++i) {
+            hashMap.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length - 2; ++i) {
+            if (nums[i] > 0) {
+                break;
+            }
+
+            for (int j = i + 1; j < nums.length - 1; ++j) {
+                int required = -1 * (nums[i] + nums[j]);
+                if (hashMap.containsKey(required) && hashMap.get(required) > j) {
+                    answer.add(Arrays.asList(nums[i], nums[j], required));
+                }
+                j = hashMap.get(nums[j]);
+            }
+
+            i = hashMap.get(nums[i]);
+        }
+
+        return answer;
+    }
     public static void print2DList(List<List<Integer>> twoDList) {
         for (List<Integer> row : twoDList) {
             for (Integer num : row) {
