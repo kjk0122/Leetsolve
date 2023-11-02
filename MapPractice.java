c07a6fb56ed44151896e11c805a689ba84d58078
kjk0122 <juke9548@gmail.com>
2023-11-02 16:29:33 +0900

    무한

--- MapPractice.java
+++ MapPractice.java
@@ -15,15 +15,47 @@ public class MapPractice {
         glossary.put("cherry", "체리");
         glossary.put("date", "데이트");
         glossary.put("grape", "포도");
-        // 사용자로부터 단어를 입력받습니다.
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("영어 단어를 입력하세요: ");
-        String inputWord = scanner.nextLine();
-        dictionary(glossary,inputWord);
-        scanner.close();
+
+        while (true) {
+            System.out.println("1. 단어 검색");
+            System.out.println("2. 새로운 단어 추가");
+            System.out.println("3. 전체 사전 출력");
+            System.out.println("4. 프로그램 종료");
+
+            System.out.print("메뉴를 선택하세요: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // 버퍼 비우기
+
+            switch (choice) {
+                case 1:
+                    System.out.print("영어 단어를 입력하세요: ");
+                    String inputWord = scanner.nextLine();
+                    dictionary(glossary, inputWord);
+                    break;
+                case 2:
+                    System.out.print("새로운 영어 단어를 입력하세요: ");
+                    String newWord = scanner.nextLine();
+                    System.out.print("단어의 뜻을 입력하세요: ");
+                    String meaning = scanner.nextLine();
+                    glossary.put(newWord, meaning);
+                    System.out.println("새로운 단어가 사전에 추가되었습니다.");
+                    break;
+                case 3:
+                    System.out.println("전체 사전: " + glossary);
+                    break;
+                case 4:
+                    System.out.println("프로그램을 종료합니다.");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("잘못된 선택입니다. 다시 선택하세요.");
+            }
+        }
     }
 
-    public static void dictionary(Map<String,String> h, String x){
+    public static void dictionary(Map<String, String> h, String x) {
         if (h.containsKey(x)) {
             System.out.println("해당 단어의 뜻: " + h.get(x));
         } else {
