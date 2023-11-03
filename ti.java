deb6d31d6843c87fb804edbc8282ee47ed9a08d8
kjk0122 <juke9548@gmail.com>
2023-11-03 19:09:12 +0900

    응애!!!!

--- ti.java
+++ ti.java
@@ -1,41 +1,46 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class ti {
 
 
     public static void main(String[] args) {
-        //"RT", "TR", "FC", "CF", "MJ", "JM", "AN", "NA"
-        String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
-        int[] choices = {5, 3, 2, 7, 5};
-        System.out.println(solution(survey, choices));
+        int x = 2;
+        int n = 5;
+        int[] lottos = {0, 0, 0, 0, 0, 0};
+        int[] win_nums = {31, 10, 45, 1, 6, 19};
+        solution2(lottos, win_nums);
     }
 
-    public static String solution(String[] survey, int[] choices) {
-        Map<String, Integer> result = new HashMap<>();
-//RT CF JM AN
-        result.put("R", 0);
-        result.put("T", 0);
-        result.put("C", 0);
-        result.put("F", 0);
-        result.put("J", 0);
-        result.put("M", 0);
-        result.put("A", 0);
-        result.put("N", 0);
+    public static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = (long) x * (i + 1);
+        }
+        return answer;
+    }
 
-        for (int i = 0; i < survey.length; i++) {
-            if (choices[i]<4) {
-                System.out.println(survey[i].charAt(0));
-            result.replace(String.valueOf(survey[i].charAt(0)),4-choices[i]);
-            }else if(choices[i]>4){
-                System.out.println(survey[i].substring(1));
-            result.replace(survey[i].substring(1),choices[i]-4);
+
+    public static void solution2(int[] lottos, int[] win_nums) {
+        int wild = 0;
+        int win = 0;
+        int[] answer = new int[2];
+        for (int j = 0; j < lottos.length; j++) {
+            if (lottos[j] == 0) {wild++;
+            } else {
+                for (int i = 0; i < win_nums.length; i++) {
+                    if (lottos[j] == win_nums[i]) win++;
+                }
             }
         }
-        StringBuilder answer=new StringBuilder();
-
-
-        System.out.println(result);
-        return "kk";
+       answer[0] = rank(win + wild);
+       answer[1] = rank(win);
+        System.out.println(Arrays.toString(answer));
+    }
+    public static int rank(int n){
+        if(n>1){
+        return 7-n;
+        }else{
+            return 6;
+        }
     }
 }
