844fad5b7cf4537832d9edecb9baafeece67974d
kjk0122 <juke9548@gmail.com>
2023-11-16 17:51:13 +0900

    업업

--- CharacterReplacement.java
+++ CharacterReplacement.java
@@ -1,12 +1,20 @@
 public class CharacterReplacement {
     public static void main(String[] args) {
-        String word = "ABCABC";
-        int num = 31;
-        int gcd = characterReplacement(word, num);
-        System.out.println(gcd);
+        String strValue = "Hello_ Java.";
+        StringBuilder sb = new StringBuilder(strValue);
+
+        int findIndex = sb.indexOf("_");
+
+        System.out.println("[변경 전]");
+        System.out.println(strValue);
+
+        if(findIndex > 0) {
+            sb.setCharAt(findIndex, ',');
+            strValue = sb.toString();
+        }
+
+        System.out.println("\n[변경 후]");
+        System.out.println(strValue);
     }
 
-    public static int characterReplacement(String s, int k) {
-    return 1;
-    }
 }
