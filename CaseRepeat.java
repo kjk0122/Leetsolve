fc628fea04623e2215879b29909cddeb17092456
kjk0122 <juke9548@gmail.com>
2023-11-29 18:12:09 +0900

    업업

--- CaseRepeat.java
+++ CaseRepeat.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class CaseRepeat {
     public static void main(String[] args) {
@@ -24,5 +26,9 @@ public class CaseRepeat {
     }
     public static String stringRemove(String my_string, String letter) {
         return my_string.replace(letter,"");
+    }
+
+    public static int[] stream(int[] numList) {
+        return IntStream.of((int) Arrays.stream(numList).filter(i -> i % 2 == 0).count(), (int) Arrays.stream(numList).filter(i -> i % 2 == 1).count()).toArray();
     }
 }
